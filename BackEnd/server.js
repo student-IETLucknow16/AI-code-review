d639901680505e1a0abc082c94a6209c38e207e3
student-IETLucknow16 <electronicsengineer2028@gmail.com>
2026-06-22 10:33:01 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,7 +7,7 @@ const app = require('./src/app')
 
 
 
-
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000");
-})
+const PORT  = process.env.PORT || 3000;
+app.listen( PORT, ()=>
+    console.log(`Server running on port ${PORT}`);
+)
